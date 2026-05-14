31a9663695780e547dd625edca0d2cfb77fedfbe
sato <matheussato1702@gmail.com>
2026-05-14 12:38:59 -0300

    Add files via upload

--- lpsesi05gg/lpsesi05gg/lpsesi05gg/src/script.js
+++ lpsesi05gg/lpsesi05gg/lpsesi05gg/src/script.js
@@ -1,10 +1,13 @@
 window.addEventListener('scroll', () => {
     const nav = document.querySelector('nav')
+    const footer = document.querySelector('footer')
 
     if(window.scrollY > 0){
         nav.classList.add('sc')
+        footer.classList.add("ft")
     }else{
         nav.classList.remove('sc')
+        footer.classList.remove("ft")
     }
 })
 
@@ -14,3 +17,4 @@ function navTo(id){
         block: 'start'
     })
 }
+
