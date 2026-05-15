d25e89d7810e3b7aac21a00b04c3ce697fae305e
sato <matheussato1702@gmail.com>
2026-05-15 09:34:27 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav')
+    const footer = document.querySelector('footer')
+
+    if(window.scrollY > 0){
+        nav.classList.add('sc')
+        footer.classList.add("ft")
+    }else{
+        nav.classList.remove('sc')
+        footer.classList.remove("ft")
+    }
+})
+
+function navTo(id){
+    document.getElementById(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+}
+
+const cards = document.querySelectorAll('#s2, .card')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.foreach((entry) => {
+        if(entry.isIntersecting){
+            cards.forEach((card,index) => {
+                setTimeout(() => {
+                    card.classList.add('visible');
+                }, index * 500)
+            });
+        }else{
+            cards.forEach((card) => {
+                card.classList.remove('visible');
+            }
+        }                                
+        });         
+    })
+}),{ threshold: 0.2 });
